78e27cd6655ca20030896d94317c798ee58a5623
LakshmiNarayanan <mlakshmi.narayanan@zoftsolutions.com>
2025-11-25 16:19:28 +0530

    Redirect URI changes

--- src/Login.tsx
+++ src/Login.tsx
@@ -16,7 +16,7 @@ const msalConfig: Configuration = {
   auth: {
     clientId: "c2c6d129-41c0-40b9-80dc-4839caed5f02",
     authority: "https://login.microsoftonline.com/a2c0c009-2f46-46d9-87b9-d5ced8c0f47b", // or your tenant
-    redirectUri: "http://localhost:5173", // 👈 your custom redirect URI
+    redirectUri: "https://rameshfly-subscription-page-msa.vercel.app/", // 👈 your custom redirect URI
     postLogoutRedirectUri: "https://www.google.com/",
   },
 };
--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ const msalConfig = {
     auth: {
       clientId: "c2c6d129-41c0-40b9-80dc-4839caed5f02", // replace with your Azure AD app client ID
       authority: "https://login.microsoftonline.com/a2c0c009-2f46-46d9-87b9-d5ced8c0f47b", // replace with your Azure AD tenant ID
-      redirectUri: "http://localhost:5173", // replace with your redirect URI
+      redirectUri: "https://rameshfly-subscription-page-msa.vercel.app", // replace with your redirect URI
     },
 };
 
